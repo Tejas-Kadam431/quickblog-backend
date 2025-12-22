1de0f80b51a519c113133c9a90b73d16175fe826
Tejas Kadam <kadamtejas548@gmail.com>
2025-12-22 13:45:46 +0530

    Add rate limiting middleware for basic API protection

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 import errorHandler from "./middleware/errorHandler.js";
+import { apiLimiter } from "./middleware/rateLimiter.js";
+
 
 
 const app = express();
@@ -14,18 +16,20 @@ await connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Bonus addition
+app.use(express.urlencoded({ extended: true }));
+app.use(apiLimiter);
+ // Bonus addition
 
 
 // Routes
+
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
+app.use(errorHandler);
 app.get('/',(req,res)=> res.send("API is Working"))
 app.get('/health', (req, res) => {
   res.status(200).json({ status: "OK", timestamp: Date.now() });
 });
-app.use('/api/admin', adminRouter)
-app.use('/api/blog', blogRouter)
-app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 3000;
 
